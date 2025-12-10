7edaf6608ba745b65543766a9e23da94bf6f3bef
Vipul-Vaibhav27 <27.vipulvaibhav2004@gmail.com>
2025-12-10 20:47:25 +0530

    Adding few comments for data objects

--- Scripts/Pokemon/pokemon_data.gd
+++ Scripts/Pokemon/pokemon_data.gd
@@ -3,14 +3,15 @@ class_name PokemonSpecies extends Resource
 @export var name: String
 @export var types: Array[TypeChart.Type] # ["Fire", "Flying"]
 @export var base_stats: Dictionary = {
-	ALIAS.HP: 78, ALIAS.ATK: 84, ALIAS.DEF: 78, 
+	ALIAS.HP: 78, ALIAS.ATK: 84, ALIAS.DEF: 78,
 	ALIAS.SPATK: 109, ALIAS.SPDEF: 85, ALIAS.SPD: 100
 }
 
 # Example: {"def": 1} for Geodude, {"spd": 2} for Pidgeotto
+# Ev refers to "Effort Value", You gain EVs by defeating enemies, if PokemonA defeats pokemonB then pokemonA will get pokemonB's xp yield value and ev yield value
 @export var ev_yield: Dictionary = {
-	ALIAS.HP: 0, ALIAS.ATK: 0, ALIAS.DEF: 0, 
+	ALIAS.HP: 0, ALIAS.ATK: 0, ALIAS.DEF: 0,
 	ALIAS.SPATK: 0, ALIAS.SPDEF: 0, ALIAS.SPD: 0
 }
 
-@export var xp_yield: int = 40 # Base XP reward
+@export var xp_yield: int = 40 # Base XP reward the opposite gains by defeating this pokemon
--- Scripts/Pokemon/pokemon_instance.gd
+++ Scripts/Pokemon/pokemon_instance.gd
@@ -5,8 +5,8 @@ var species: PokemonSpecies
 var level: int = 1
 var nature_multiplier: Dictionary = {ALIAS.ATK: 1.1, ALIAS.SPATK: 0.9} # Example: Adamant
 
-var ivs: Dictionary = {} 
-var evs: Dictionary = {}
+var ivs: Dictionary = {}	# Genetic Stat
+var evs: Dictionary = {}	# Stat gained through training - Permanent Stat to be stored with json
 var current_stats: Dictionary = {} # The final calculated values
 var current_hp: int
 
@@ -19,12 +19,12 @@ const STAT_KEYS = [ALIAS.HP, ALIAS.ATK, ALIAS.DEF, ALIAS.SPATK, ALIAS.SPDEF, ALI
 func _init(p_species: PokemonSpecies, p_level: int):
 	species = p_species
 	level = p_level
-	
+
 	# Initialize
 	for key in STAT_KEYS:
 		evs[key] = 0
 		ivs[key] = randi_range(0, MAX_IV) # Randomize "Genes" (0-31)
-	
+
 	recalculate_stats()
 	current_hp = current_stats[ALIAS.HP] # Heal to full on create
 
@@ -40,7 +40,7 @@ func _calc_hp(stat: String) -> int:
 	var base = species.base_stats[stat]
 	var iv = ivs[stat]
 	var ev = evs[stat]
-	
+
 	# Formula: ((2 * Base + IV + (EV/4)) * Level / 100) + Level + 10
 	var core = (2 * base + iv + (ev / 4))
 	var result = int((core * level) / 100.0) + level + 10
@@ -50,34 +50,34 @@ func _calc_stat(stat: String) -> int:
 	var base = species.base_stats[stat]
 	var iv = ivs[stat]
 	var ev = evs[stat]
-	
+
 	# Formula: (((2 * Base + IV + (EV/4)) * Level / 100) + 5) * Nature
 	var core = (2 * base + iv + (ev / 4))
 	var result = int((core * level) / 100.0) + 5
-	
+
 	# Apply Nature Multiplier (1.1, 0.9, or 1.0)
 	if nature_multiplier.has(stat):
 		result = int(result * nature_multiplier[stat])
-		
+
 	return result
 
 # --- Training Logic ---
 func gain_ev(stat: String, amount: int):
 	var total_evs = 0
-	for key in evs: 
+	for key in evs:
 		total_evs += evs[key]
-	
+
 	if total_evs >= MAX_EV_TOTAL:
 		print("EVs are fully maxed out!")
 		return
 
 	var current_val = evs[stat]
 	var space_left = MAX_EV_PER_STAT - current_val
-	
+
 	var actual_add = min(amount, space_left)
-	
+
 	actual_add = min(actual_add, MAX_EV_TOTAL - total_evs)
-	
+
 	if actual_add > 0:
 		evs[stat] += actual_add
 		recalculate_stats()
@@ -90,7 +90,7 @@ func gain_ev(stat: String, amount: int):
 class MoveSlot:
 	var move_data: Move
 	var current_pp: int
-	
+
 	func _init(m: Move):
 		move_data = m
 		current_pp = m.max_pp
