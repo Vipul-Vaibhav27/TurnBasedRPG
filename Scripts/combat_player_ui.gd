9dd7c80a4353d192c15d309dcc16591d10df8aed
whocares-abt <126311601+whocares-abt@users.noreply.github.com>
2025-12-10 13:08:06 +0530

    Connected buttons and UI via signals

--- Scripts/combat_player_ui.gd
+++ Scripts/combat_player_ui.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var action_container = $VBoxContainer
+@onready var action_container = $ActionContainer
 @onready var menu_item = preload("res://Scenes/menu_item.tscn")
 
 # Called when the node enters the scene tree for the first time.
@@ -8,22 +8,49 @@ func _ready() -> void:
 	var action_file_path = "res://Data/player_skills.json"
 	var actions = load_parse_json(action_file_path)
 	
-	for action in actions:
-		var action_button = menu_item.instantiate()
-		action_container.add_child(action_button)
-		
-		if (action_button.has_method("update_text")):
-			action_button.update_text(action)
-		else:
-			printerr("Unable to update text")
-		
+	create_menu(actions, Vector2(250,40), action_container)
 	action_container.grab_focus()
 
-func UI_loop():
-	pass
+# Functions for getting and processing player actions
 
+func player_pressed_button(button_name):
+	print("Player has pressed ", button_name)
 
-func load_parse_json(file_path) -> Variant:
+# Functions for creating a menu
+
+func create_menu(items : Variant, item_size : Vector2, menu_container : Container) -> void:
+	delete_all_children(menu_container)
+
+	for item in items:
+		var item_button = menu_item.instantiate()
+		menu_container.add_child(item_button)
+		
+		if (item_button.has_method("update_text")):
+			item_button.update_text(item)
+		else:
+			printerr("No method to update text")
+			
+		if (item_button.has_method("update_size")):
+			item_button.update_size(item_size)
+		else:
+			printerr("No method to update size")
+			
+		if (item_button.has_signal("menu_item_picked")):
+			var pressed = player_pressed_button.bind(item)
+			var err = item_button.menu_item_picked.connect(pressed)
+			if (err != OK):
+				printerr("Unable to connect. Error: ", error_string(err))
+		else:
+			printerr("No signal to notify menu item picked")
+
+func delete_all_children(node : Node):
+	for child in node.get_children():
+		node.remove_child(child)
+		child.queue_free()
+
+# Functions for loading and parsing jsons
+
+func load_parse_json(file_path : String) -> Variant:
 	var json_string = load_file_contents(file_path)
 	var json_parsed = parse_json(json_string)
 	return json_parsed
--- Scripts/menu_item.gd
+++ Scripts/menu_item.gd
@@ -4,9 +4,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	update_size(Vector2(250, 40))
-	update_text("FILLER")
-	update_font_size(25)
+	pass
 
 func update_size(new_size_vector : Vector2) -> void:
 	button.set_size(new_size_vector)
@@ -14,5 +12,7 @@ func update_size(new_size_vector : Vector2) -> void:
 func update_text(new_text : String) -> void:
 	button.text = new_text
 
-func update_font_size(new_size : int) -> void:
-	button.add_theme_font_size_override("normal_font_size", new_size)
+signal menu_item_picked
+
+func _on_button_pressed() -> void:
+	menu_item_picked.emit()
