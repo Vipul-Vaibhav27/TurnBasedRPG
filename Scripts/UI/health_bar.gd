b0df3c2461d3e61b1f3bfa84d856c4ce7ae6f5b6
whocares-abt <126311601+whocares-abt@users.noreply.github.com>
2025-12-13 18:22:10 +0530

    Position hp bars

--- Scripts/UI/health_bar.gd
+++ Scripts/UI/health_bar.gd
@@ -2,5 +2,5 @@ extends Control
 
 @onready var hp_progress_bar = $TextureProgressBar
 
-func update_hp(curr_hp, max_hp) -> void:
+func update_hp(curr_hp : float, max_hp : float) -> void:
 	hp_progress_bar.value = (curr_hp/max_hp) * hp_progress_bar.max_value
